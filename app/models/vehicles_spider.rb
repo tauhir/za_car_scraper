5f4c9e886a1c8be195556c134f80951129a44968
tauhir Edwards <tauhir.edwards@gmail.com>
2020-12-06 14:22:14 +0200

    updated parsing so I get proper data

--- app/models/vehicles_spider.rb
+++ app/models/vehicles_spider.rb
@@ -9,25 +9,49 @@ class VehiclesSpider < Kimurai::Base
 
     def parse(response, url:, data: {})
       response.xpath("//div[@class='item clearfix vehicle-list__item']").each do |vehicle|
-        parse_page(vehicle)
+          request_to :parse_individual, url: absolute_url(vehicle.css('a.vehicle-list__view-vehicle')[0][:href], base: url)
       end
       if next_page = response.at_xpath("//a[@class='pagination__page pagination__nav js-pagination fa fa-right-open-big']")
-        return if next_page[:href][-2,2] == "20" # limits the pages to page 20 for testing and dev while I implement the cassettes gem
+        return if next_page[:href][-1] == "3" # limits the pages to page 3 for testing and dev while I implement the cassettes gem
         request_to :parse, url: absolute_url(next_page[:href], base: url)
       end
     end
 
-    def parse_page(vehicle)
+    def parse_individual(response, url:, data: {}) 
+      response.xpath("//div[@class='box vehicledetails']").each do |vehicle|
+        parse_page(vehicle, url)
+      end
+    end
+
+    def parse_page(vehicle,url)
       item = {}
-      item[:title]      = vehicle.css('a.vehicle-list__vehicle-name')&.text&.squish
-      item[:year]      = vehicle.css('li.vehicle-list__ie7icon-0')&.text&.squish&.delete('^0-9').to_i
-      item[:price] = vehicle.css('span.price')&.text&.squish&.delete('^0-9').to_i
-      item[:miles]      = vehicle.css('li.vehicle-list__ie7icon-1')&.text&.squish&.delete('^0-9').to_i
-      item[:transmission] = vehicle.css('li.vehicle-list__ie7icon-2')&.text&.squish.gsub('Transmission: ', '')
-      item[:fuel_type] = vehicle.css('li.vehicle-list__ie7icon-3')&.text&.squish.gsub('Fuel Type: ', '')
-      item[:location]  = vehicle.css('i.agent-locality')&.text&.squish.gsub('Location: ', '')
-      item[:url] = vehicle.css('a.vehicle-list__view-vehicle')[0][:href]
+      item[:title]      = vehicle.css('h3.vehicle-view__description-heading')&.text&.squish
+      item[:price] = vehicle.css('div.vehicle-view__price')&.text&.squish&.delete('^0-9').to_i
+      # data is stored in tables so we need to extract it
+      table_items = []
+      table = vehicle.at('.vehicle-view__section')
+      table.search('tr').each do |row|
+        row.search('td').each do |attrib| 
+          table_items.append(attrib.children.text)
+        end
+      end
+      item[:year]      = get_next_in_array(table_items, "Year").squish&.delete('^0-9').to_i
+      item[:miles]      = get_next_in_array(table_items, "Mileage").squish&.delete('^0-9').to_i
+      item[:transmission] = get_next_in_array(table_items, "Transmission")&.squish
+      item[:fuel_type] = get_next_in_array(table_items, "Fuel Type")&.squish
+      item[:location]  = get_next_in_array(table_items, "Area")&.squish
+      item[:color]  = get_next_in_array(table_items, "Colour")&.squish
+      # item[:condition]  = get_next_in_array(table_items, "Condition")&.squish TODO
+      # item[:reference]  = get_next_in_array(table_items, "Reference")&.squish
+      # item[:options]  = get_next_in_array(table_items, "Options")&.squish
+      item[:url] = url
       Vehicle.where(item).first_or_create
     end
   end
+
+  def get_next_in_array(array,value)
+    index = array.find_index(value)
+    array[index+1]
+  end
+
   
