8a5bdf993da9cf22d3e5191d438cb43dc59afb7b
Tauhir Edwards <tauhir.edwards@gmail.com>
2021-02-04 13:39:15 +0200

    corrected vehicle filter to be future proof

--- app/models/vehicle_filter_spider.rb
+++ app/models/vehicle_filter_spider.rb
@@ -10,12 +10,15 @@ class VehicleFilterSpider < Kimurai::Base
   end
  
   def parse(response, url:, data: {})
-    byebug
     list = browser.find("//div[@id='make_model_button']").all("//li[@class='item']")
     x = list[0].all('//a').first.all('//span')
     names_list = x.map{|value| value.text}
     makes_list = x[names_list.find_index("Makes")+1,(names_list.find_index("Body Types")-names_list.find_index("Makes")-1)]
-    body_type_list = x[names_list.find_index("Body Types")+1,(names_list.find_index("R 20 000") - names_list.find_index("Body Types")-1)]
+    body_list_start = names_list.find_index("Body Types")+1
+    body_list_end = names_list[body_list_start,names_list.size].each_with_index do |string,index| break index if  x[0,2] == "R " end # need to do this because its destined to fail if I use fixed values as before. 
+    # the end of the body_list is a number that is the cheapest car on the market which can change
+    body_type_list = x[body_list_start,(body_list_end - 2)] 
+    
     browser.find("//div[@id='make_model_button']").click
     makes_list.each do |maker|
 
